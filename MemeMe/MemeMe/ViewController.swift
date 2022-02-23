d751ba756e7e8611482055ba8ea80e7acb1f677b
Ashley Thornton <athorn2679@gmail.com>
2022-02-22 23:19:28 -0800

    Set text attributes

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -6,20 +6,56 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var selectImageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            .strokeColor: UIColor.black,
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            .strokeWidth: 3.0
+        ]
+        topTextField.text = "TOP"
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.delegate = self
+        topTextField.clearsOnBeginEditing = true
+        topTextField.textAlignment = .center
+        
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.delegate = self
+        bottomTextField.clearsOnBeginEditing = true
+        bottomTextField.textAlignment = .center
+        
+//        [topTextField, bottomTextField].forEach { tf in
+//            guard let tf = tf else { return }
+//            tf.textAlignment = .center
+//            tf.defaultTextAttributes = memeTextAttributes
+//            tf.delegate = self
+//        }
+//
+//        for tf in [topTextField, bottomTextField] {
+//            guard let tf = tf else { break }
+//            tf.textAlignment = .center
+//            tf.defaultTextAttributes = memeTextAttributes
+//            tf.delegate = self
+//        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+//        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
+    
+    // MARK: - UIImagePickerControllerDelegate
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             selectImageView.image = image
@@ -29,8 +65,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    
     }
     
+    // MARK: - UITextFieldDelegate
+    
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        textField.text = nil
+//    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
     @IBAction func selectImageFromAlbum(_ sender: Any) {
         let imagePicker = UIImagePickerController()
@@ -41,10 +88,34 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func selectImageFromCamera(_ sender: Any) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .camera
-        present (imagePicker, animated: true, completion: nil)
+        checkCameraAuthorization { isAuthorized in
+            if isAuthorized {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = .camera
+                self.present (imagePicker, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func checkCameraAuthorization(thenPerform completion: @escaping (Bool) -> Void) {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            print("Capture access already granted")
+            return
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { accessIsGranted in
+                print("Capture access granted: \(accessIsGranted)")
+                completion(accessIsGranted)
+            }
+        case .denied:
+            print("Capture access denied")
+        case .restricted:
+            print("Capture access restricted")
+        @unknown default:
+            fatalError()
+        }
+    
         
     }
     
